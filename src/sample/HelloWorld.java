6b7d6b48f140385e8154e6ecae7086fc9a20d6ea
Barry Mullan <bmullan@rallydev.com>
2016-12-10 14:23:16 -0500

    US98 Removed duplicate code.

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -10,13 +10,7 @@ public class HelloWorld {
     public int add(int a, int b) {
     	return a+b;
     }
-    
-   
-
-    public int add2(int a, int b) {
-    	return a+b;
-    }
-    
+        
     public int add3(int a, int b) {
     	return a+b;
     }
