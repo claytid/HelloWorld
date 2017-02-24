a80d00b1607382d7c689969ff23522ef3a69a43b
Barry Mullan <mullan@gmail.com>
2017-02-23 21:22:37 -0500

    changes

---
+++ test/sample/HelloWorldRealTest.java
@@ -0,0 +1,31 @@
+package sample;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.BeforeClass;
+
+import sample.HelloWorld;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+public class HelloWorldRealTest {
+
+    private static HelloWorld hw = null;
+
+    @BeforeClass
+    public static void setup() {
+        hw = new HelloWorld();
+    }
+    
+    @Test
+    public void testAdd1() {
+       assertEquals( hw.add(1,1),2);
+    }
+}
